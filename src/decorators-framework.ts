2e33b1f6effad87fbe089e61f9830abe2cbd9aab
Federico <tacchinifederico@gmail.com>
2021-06-01 19:41:53 +0200

    Adding defaults when no properties or routes

--- src/decorators-framework.ts
+++ src/decorators-framework.ts
@@ -31,7 +31,7 @@ export class DecoratorsFramework implements FlexibleFramework {
     }
 
     public async createPipelineDefinitions(): Promise<FlexiblePipelineDocument[]> {
-        let candidateControllers = await this.controllerLoader.loadControllers();
+        let candidateControllers = await this.controllerLoader.loadControllers() || [];
 
         let controllers = candidateControllers.filter(candidateController => {
             return this.hasMetadata(CONTROLLER_KEY, candidateController);
@@ -52,7 +52,7 @@ export class DecoratorsFramework implements FlexibleFramework {
         target: Type<any>,
         controllerDefinition: ControllerDefinition<FlexibleFilter | undefined>): FlexiblePipelineDocument[] {
 
-        let candidateRoutes = Object.getOwnPropertyNames(target.prototype);
+        let candidateRoutes = Object.getOwnPropertyNames(target.prototype) || [];
 
         let routes = flatten(candidateRoutes.filter(candidateRoute => {
             return this.hasMetadata(ROUTE_KEY, target, candidateRoute);
@@ -93,7 +93,7 @@ export class DecoratorsFramework implements FlexibleFramework {
             return [[], []];
         }
 
-        let middlewareDefinitions: MiddlewareDefinition<object>[] = this.getMetadata(MIDDLEWARE_KEY, target, property);
+        let middlewareDefinitions: MiddlewareDefinition<object>[] = this.getMetadata(MIDDLEWARE_KEY, target, property) || [];
 
         return middlewareDefinitions
             .map(middlewareDefinition => {
@@ -126,7 +126,7 @@ export class DecoratorsFramework implements FlexibleFramework {
         route: string,
         controllerDefinition: ControllerDefinition<FlexibleFilter>): (FlexibleFilterRecipe<FlexibleFilter> | FlexibleFilterRecipe<FlexibleFilter>[])[] {
 
-        let routeDefinitions: RouteDefinition<FlexibleFilter | undefined>[] = this.getMetadata(ROUTE_KEY, target, route);
+        let routeDefinitions: RouteDefinition<FlexibleFilter | undefined>[] = this.getMetadata(ROUTE_KEY, target, route) || [];
         let filterStack: (FlexibleFilterRecipe<FlexibleFilter> | FlexibleFilterRecipe<FlexibleFilter>[])[] = [];
 
         if (controllerDefinition.filter) {
@@ -147,7 +147,7 @@ export class DecoratorsFramework implements FlexibleFramework {
     private createExtractorRecipes(target: Type<any>, property: string): {
         [paramIndex: number]: FlexibleRecipe<FlexibleExtractor> | FlexibleRecipe<FlexibleExtractor>[];
     } {
-        let extractorDefinitions: ExtractorDefinition<FlexibleExtractor>[] = this.getMetadata(EXTRACTOR_KEY, target, property);
+        let extractorDefinitions: ExtractorDefinition<FlexibleExtractor>[] = this.getMetadata(EXTRACTOR_KEY, target, property) || [];
 
         let extractors:  {
             [paramIndex: number]: FlexibleRecipe<FlexibleExtractor> | FlexibleRecipe<FlexibleExtractor>[];
